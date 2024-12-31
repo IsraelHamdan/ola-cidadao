876fc61eea317a0933019ea94d14b49eb5cd1eae
Mateus Santos <mateussilvanzg@gmail.com>
2024-12-31 14:22:06 -0300

    main-content criado

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
 import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarLeftComponent, SidebarRightComponent],
+  imports: [RouterOutlet, SidebarLeftComponent, SidebarRightComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
