8cc8c4e72223fd7d2176c4bf160f94646a26114b
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-08 16:35:01 -0300

    estilização de manifestação inicializada

--- front-end/src/app/main-content/main-content.component.ts
+++ front-end/src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ManifestationComponent } from "../components/manifestation/manifestation.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [ManifestationComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.sass'
 })
