b55b27d9fc89df92b73008ff2b765b5ae0c9626e
Víctor Ramaciotti <victtordan10@gmail.com>
2025-01-10 13:38:16 -0300

    adição da opção de login

--- front-end/src/app/components/sidebar-left/sidebar-left.component.ts
+++ front-end/src/app/components/sidebar-left/sidebar-left.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService, User } from '@auth0/auth0-angular';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-left',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AsyncPipe],
   templateUrl: './sidebar-left.component.html',
   styleUrl: './sidebar-left.component.sass',
 })
-export class SidebarLeftComponent {}
+export class SidebarLeftComponent {
+
+  constructor(public auth: AuthService) {}
+  user?: User | undefined | null;
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((user) => {
+      this.user = user;
+    });
+}
+
+}
