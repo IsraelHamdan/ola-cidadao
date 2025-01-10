bc8a5e5ecd73a80cadd9e08a72861ceb04db9d1a
Víctor Ramaciotti <victtordan10@gmail.com>
2025-01-10 13:41:57 -0300

    assegura que o usuário esteja logado para acessar perfil

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -7,10 +7,11 @@ import { MetricasComponent } from './pages/metricas/metricas.component';
 import { PrefeituraComponent } from './pages/prefeitura/prefeitura.component';
 import { ConfiguracoesComponent } from './pages/configuracoes/configuracoes.component';
 import { NovaManifestacaoComponent } from './pages/nova-manifestacao/nova-manifestacao.component';
+import { authGuardFn } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
   { path: '', component: FeedComponent },
-  { path: 'user', component: UserComponent },
+  { path: 'user', component: UserComponent, canActivate: [authGuardFn] },
   { path: 'metricas', component: MetricasComponent },
   { path: 'prefeitura', component: PrefeituraComponent },
   { path: 'configuracoes', component: ConfiguracoesComponent },
