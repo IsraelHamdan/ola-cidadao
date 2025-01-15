787ea9a283a45736e31512c278a86eda7929c61e
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-14 22:52:01 -0300

    iniciada a lógica do serviço de manifestação

--- front-end/src/services/manifestacoes/manfestacoes.service.ts
+++ front-end/src/services/manifestacoes/manfestacoes.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Manifestacao } from '../../interfaces/Manifestacao';
+import { enviroments } from '../../enviroments';
+import { ResponseManifestacao } from '../../interfaces/ResponseManifestacao';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ManfestacoesService {
+  private baseApiUrl = enviroments.baseURL;
+  private apiUrl = `${this.baseApiUrl}/manifestacoes/`;
+  allManifestations!: Manifestacao[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllManifestations(): Observable<ResponseManifestacao<Manifestacao[]>> {
+    return this.http.get<ResponseManifestacao<Manifestacao[]>>(this.apiUrl);
+  }
 }
