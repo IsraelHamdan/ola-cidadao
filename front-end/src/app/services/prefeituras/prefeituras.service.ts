0582739cd577e2d73d6dbe672566661201e72c80
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-18 11:34:37 -0300

    feat(serviço prefeitura): função getPrefeitura adicionada

--- front-end/src/app/services/prefeituras/prefeituras.service.ts
+++ front-end/src/app/services/prefeituras/prefeituras.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Prefeitura } from '../../../interfaces/prefeituraDTO';
+environment;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PrefeiturasService {
+  private baseUrl = environment.baseURL;
+  private apiUrl = `${this.baseUrl}/prefeituras`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPrefeitura(id: number): Observable<Prefeitura> {
+    return this.http.get<Prefeitura>(`${this.apiUrl}/${id}/`);
+  }
 }
