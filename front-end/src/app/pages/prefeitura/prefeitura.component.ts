96615e572fdac4d96fd16994f12bd33e006b0006
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-18 11:32:05 -0300

    feat(componente prefeitura): implementada função para consumir serviço

--- front-end/src/app/pages/prefeitura/prefeitura.component.ts
+++ front-end/src/app/pages/prefeitura/prefeitura.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CidadaoDTO } from '../../../interfaces/CidadaoDTO';
+import { PrefeiturasService } from '../../services/prefeituras/prefeituras.service';
+import { Prefeitura } from '../../../interfaces/prefeituraDTO';
 
 @Component({
   selector: 'app-prefeitura',
   standalone: true,
   imports: [],
   templateUrl: './prefeitura.component.html',
-  styleUrl: './prefeitura.component.sass'
+  styleUrl: './prefeitura.component.sass',
 })
-export class PrefeituraComponent {
+export class PrefeituraComponent implements OnInit {
+  prefeitura!: Prefeitura;
 
-  cidade: string = "teste";
+  cidade: string = 'teste';
   // img_perfil: string = "";
   // img_background: string = "";
 
+  constructor(private prefeituraService: PrefeiturasService) {}
+
+  ngOnInit(): void {
+    this.prefeituraService.getPrefeitura(2).subscribe((prefeitura) => {
+      this.prefeitura = prefeitura;
+    });
+  }
+
+  getPrefeitura() {
+    const user: CidadaoDTO = JSON.parse(localStorage.getItem('user')!);
+  }
 }
