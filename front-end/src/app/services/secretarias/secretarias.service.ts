e577ac292321f2bddbe4fbae0588313b13663c6f
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-19 20:59:57 -0300

    feat: serviço de secretaria implementado

--- front-end/src/app/services/secretarias/secretarias.service.ts
+++ front-end/src/app/services/secretarias/secretarias.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Secretaria } from '../../../interfaces/SecretariaDTO';
+import { ResponseSecretaria } from '../../../interfaces/ResponseSecretaria';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SecretariasService {
+  baseUrl = environment.baseURL;
+  apiUrl = `${this.baseUrl}/orgaos`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllSecretarias(): Observable<ResponseSecretaria<Secretaria[]>> {
+    return this.http.get<ResponseSecretaria<Secretaria[]>>(`${this.apiUrl}/`);
+  }
+
+  getSecretaria(id: number): Observable<ResponseSecretaria<Secretaria>> {
+    return this.http.get<ResponseSecretaria<Secretaria>>(
+      `${this.apiUrl}/${id}`
+    );
+  }
 }
