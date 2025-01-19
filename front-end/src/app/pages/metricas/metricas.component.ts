4bc652e92c246e4aa8a69aa76da14a045b820aec
Víctor Ramaciotti <victtordan10@gmail.com>
2025-01-18 22:23:37 -0300

    fix: correções na página de métricas

--- front-end/src/app/pages/metricas/metricas.component.ts
+++ front-end/src/app/pages/metricas/metricas.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 import * as echarts from 'echarts/core';
 import { EChartsCoreOption } from 'echarts/core';
-import { BarChart } from 'echarts/charts';
-import { GridComponent } from 'echarts/components';
+import { BarChart, PieChart } from 'echarts/charts';
+import { GridComponent, LegendComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
-echarts.use([BarChart, GridComponent, CanvasRenderer]);
+echarts.use([BarChart, GridComponent, CanvasRenderer, PieChart, LegendComponent]);
 
 @Component({
   selector: 'app-metricas',
@@ -20,7 +20,7 @@ echarts.use([BarChart, GridComponent, CanvasRenderer]);
 })
 export class MetricasComponent {
 
-  chartOption: EChartsCoreOption = {
+  secretariasBar: EChartsCoreOption = {
   
     xAxis:{
       type: 'value',
@@ -35,9 +35,8 @@ export class MetricasComponent {
     },
 
     legend: {
-      data: '2024',
-      bottom: 0, 
-      left: 'center',
+      data: ['2024'],
+      show: true
     },
 
     series: [
@@ -61,6 +60,43 @@ export class MetricasComponent {
       bottom: '30%',  
       top: '10%'
     }
+
+  }
+
+  gastosPie: EChartsCoreOption = {
+
+    legend: {
+      bottom: 10,
+      left: 'center',
+      data: ['Saúde', 'Educação', 'Infraestrutura', 'Comunicação', 'Cultura'],
+      show: true
+    },
+
+    series: [
+      {
+        type: 'pie',
+        radius: [60, 140],
+        roseType: 'radius',
+        label: {
+          show: false
+        },
+        data: [
+          { value: 158, name: 'Saúde' },
+          { value: 256, name: 'Educação' },
+          { value: 235, name: 'Infraestrutura' },
+          { value: 132, name: 'Comunicação' },
+          { value: 321, name: 'Cultura' }
+
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ],
 
   }
 
