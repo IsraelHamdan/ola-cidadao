e756fe6099ed1573d6e1efdeb2339abf3a341ad3
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-20 20:28:41 -0300

    feat: butão para exibir mais secretarias

--- front-end/src/app/components/sidebar-right/sidebar-right.component.ts
+++ front-end/src/app/components/sidebar-right/sidebar-right.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-right',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar-right.component.html',
-  styleUrl: './sidebar-right.component.sass'
+  styleUrl: './sidebar-right.component.sass',
 })
 export class SidebarRightComponent {
+  tags: boolean = false;
 
+  moreTags() {
+    this.tags = !this.tags;
+  }
 }
