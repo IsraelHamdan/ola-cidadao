fa3d5f318770d886410dfeee7db89c1738d01a65
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-21 15:06:16 -0300

    feat: interface resposta implementada

---
+++ front-end/src/interfaces/Resposta.ts
@@ -0,0 +1,9 @@
+export interface Resposta {
+  id: number;
+  conteudo: string;
+  manifestacao: number;
+  usuario: number;
+  nome_usuario: string;
+  tipo_usuario: string;
+  data_criacao: string;
+}
