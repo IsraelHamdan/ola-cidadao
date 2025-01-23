8c18c75679fba3621cfa94a1b4e364d857c21807
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-22 22:02:40 -0300

    feat: edição de conteúdo da manifestação implementada

--- front-end/src/app/components/edit-manifestation/edit-manifestation.component.ts
+++ front-end/src/app/components/edit-manifestation/edit-manifestation.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ManfestacoesService } from '../../services/manifestacoes/manfestacoes.service';
+import { Manifestacao } from '../../interfaces/Manifestacao';
 
 @Component({
   selector: 'app-edit-manifestation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-manifestation.component.html',
-  styleUrl: './edit-manifestation.component.sass'
+  styleUrl: './edit-manifestation.component.sass',
 })
-export class EditManifestationComponent {
+export class EditManifestationComponent implements OnInit {
+  @Input() isOpen: boolean = false;
+  @Input() close: () => void = () => {};
+  @Input() manifestation!: Manifestacao;
 
+  manifestacaoCurrent!: Manifestacao;
+  formManifestationEdit!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private manifestationService: ManfestacoesService
+  ) {}
+
+  ngOnInit(): void {
+    this.formManifestationEdit = this.fb.group({
+      conteudo: [''],
+      imagem: [null],
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['manifestation'] && this.manifestation) {
+      this.formManifestationEdit.setValue({
+        conteudo: this.manifestation.conteudo || '',
+        imagem: null,
+      });
+    }
+  }
+
+  onSubmit() {
+    const formValue = this.formManifestationEdit.value;
+    const formData = new FormData();
+
+    formData.append('conteudo', formValue.conteudo);
+
+    const imagemInput = (document.getElementById('imagem') as HTMLInputElement)
+      .files;
+
+    if (imagemInput && imagemInput.length > 0) {
+      formData.append('imagem', formValue.imagem);
+    }
+
+    this.manifestationService
+      .editManifestation(this.manifestation.id, formData)
+      .subscribe({
+        next: (response) => {
+          console.log('Atulizado', response);
+          this.manifestationService.manifestationCreated.emit();
+          this.close();
+        },
+        error: (err) => {
+          console.log('Erro na atualizacao:', err);
+        },
+      });
+  }
+
+  alterarImagem() {
+    const inputChangeImage = document.getElementById('imagem');
+    inputChangeImage!.click();
+  }
 }
